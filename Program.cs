36ee0b70f697e19a8aa6a4803b44ff57bcd56b0b
sergio351 <velazquezsergio714@gmail.com>
2022-05-08 20:34:12 -0300

    primercommit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaVector2
+{
+    class PruebaVector2
+    {
+        private float[] alturas;
+        private float promedio;
+
+        public void Cargar() 
+        {
+            alturas=new float[5];
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese la altura de la persona:");
+                string linea = Console.ReadLine();
+                alturas[f] = float.Parse(linea);
+            }
+        }
+    
+        public void CalcularPromedio() 
+        {
+            float suma;
+            suma=0;
+            for(int f=0; f < 5; f++) 
+            {
+                suma=suma+alturas[f];
+            }
+            promedio=suma/5;
+            Console.WriteLine("Promedio de alturas:"+promedio);
+        }
+
+        public void MayoresMenores() 
+        {
+            int may,men;
+            may=0;
+            men=0;
+            for(int f = 0; f < 5; f++) 
+            {
+                if (alturas[f] > promedio) 
+                {
+	                may++;
+                }
+                else
+                {
+                    if (alturas[f] < promedio) 
+                    {
+                        men++;
+                    }
+                }
+            }
+            Console.WriteLine("Cantidad de personas mayores al promedio:"+may);
+            Console.WriteLine("Cantidad de personas menores al promedio:"+men);
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector2 pv2 = new PruebaVector2();
+            pv2.Cargar();
+            pv2.CalcularPromedio();
+            pv2.MayoresMenores();
+        }
+    }
+}
